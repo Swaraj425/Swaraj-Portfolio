5f1430108efb941491494af1e63fa7b34c2341bf
Swaraj Phalke <swarajphalke425@gmail.com>
2025-02-24 13:13:44 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
         e.preventDefault();
 
         try {
-            const { data } = await axios.post(`https://swaraj-portfolio-backend.vercel.app/api/contact`, {
+            const { data } = await axios.post(`https://swaraj-portfolio-backend.vercel.app//api/contact`, {
                 firstName,
                 lastName,
                 email,
